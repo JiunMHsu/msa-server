8fd6c0bd3f6695e9bf422285639d7913aa11552a
Jiun Ming Hsu <hsujm219@gmail.com>
2024-11-08 00:56:07 -0300

    cleanup

--- src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './response/http.response';
-export * from './router/router';
-export * from './repository/database';
-export * from './utilities/jwtHandler.utility';
-export * from './utilities/streamer.utility';
-export * from './models/preview.model';
--- src/shared/utilities/auth.utility.ts
+++
@@ -1,3 +0,0 @@
-export class Authenticator {}
-
-export class Authorizer {}
