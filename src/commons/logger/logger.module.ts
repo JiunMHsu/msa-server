ccf6dda743e960d80dc542de03de2e0e38eb70d3
Jiun Ming Hsu <hsujm219@gmail.com>
2025-02-11 04:05:32 -0300

    ~

--- src/commons/logger/logger.module.ts
+++ src/commons/logger/logger.module.ts
@@ -1,17 +1,5 @@
-import { Global, Logger, Module, Provider } from '@nestjs/common';
-import { LogLevel } from '@nestjs/common/services/logger.service';
-import { ConfigService } from '@nestjs/config';
-
-const loggerProvider: Provider = {
-    provide: Logger,
-    useFactory: (configService: ConfigService) => {
-        const level = configService.get<LogLevel>('LOGGER_LEVEL', 'log');
-        const logger = new Logger();
-        logger.localInstance.setLogLevels?.([level]);
-        return logger;
-    },
-    inject: [ConfigService],
-};
+import { Global, Module } from '@nestjs/common';
+import { loggerProvider } from './logger.provider';
 
 @Global()
 @Module({
